c9342c9825e9051ec3d16253f1b6eda36c869ef4
Hugo LORENT <hugo.lorent@protonmail.com>
2025-04-29 17:18:18 +0200

    add basic HTTP request parsing and routing

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::io::Write;
+use std::io::{BufRead, BufReader, Write};
 #[allow(unused_imports)]
 use std::net::TcpListener;
 
@@ -7,9 +7,18 @@ fn main() {
 
     for stream in listener.incoming() {
         match stream {
-            Ok(mut _stream) => {
+            Ok(mut stream) => {
                 println!("accepted new connection");
-                _stream.write(b"HTTP/1.1 200 OK\r\n\r\n").unwrap();
+                let reader = BufReader::new(&stream);
+                let request_line = reader.lines().next().unwrap().unwrap();
+
+                let status_line = match request_line.as_str() {
+                    "GET / HTTP/1.1" => "HTTP/1.1 200 OK",
+                    _ => "HTTP/1.1 404 Not Found",
+                };
+
+                let response = format!("{status_line}\r\n\r\n");
+                stream.write(response.as_bytes()).unwrap();
             }
             Err(e) => {
                 println!("error: {}", e);
