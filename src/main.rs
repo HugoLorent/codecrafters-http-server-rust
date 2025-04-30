e6b3ebdd884827ae1fb163d6120ceb40bdf8b713
Hugo LORENT <hugo.lorent@protonmail.com>
2025-04-30 13:22:06 +0200

    add file serving functionality

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@
 use std::net::TcpListener;
 use std::{
     collections::HashMap,
+    env, fs,
     io::{BufRead, BufReader, Write},
     net::TcpStream,
     thread,
@@ -11,6 +12,7 @@ const HTTP_OK: &str = "HTTP/1.1 200 OK";
 const HTTP_NOT_FOUND: &str = "HTTP/1.1 404 Not Found";
 const HTTP_BAD_REQUEST: &str = "HTTP/1.1 400 Bad Request";
 const CONTENT_TYPE_PLAIN: &str = "Content-Type: text/plain";
+const CONTENT_TYPE_OCTET_STREAM: &str = "Content-Type: application/octet-stream";
 const CRLF: &str = "\r\n";
 
 fn main() {
@@ -65,6 +67,7 @@ fn handle_connection(mut stream: TcpStream) -> std::io::Result<()> {
             ("GET", "/") => format!("{}{}{}{}", HTTP_OK, CRLF, CRLF, CRLF),
             ("GET", p) if p.starts_with("/echo/") => handle_echo_endpoint(p),
             ("GET", "/user-agent") => handle_user_agent_endpoint(&headers),
+            ("GET", p) if p.starts_with("/files") => handle_file_endpoint(path),
             _ => format!("{}{}{}", HTTP_NOT_FOUND, CRLF, CRLF),
         }
     } else {
@@ -104,5 +107,33 @@ fn handle_user_agent_endpoint(headers: &HashMap<String, String>) -> String {
             user_agent
         ),
         None => format!("{}{}{}", HTTP_BAD_REQUEST, CRLF, CRLF),
+    }
+}
+
+fn handle_file_endpoint(path: &str) -> String {
+    let args: Vec<String> = env::args().collect();
+    if args.len() <= 2 {
+        return format!("{}{}{}", HTTP_BAD_REQUEST, CRLF, CRLF);
+    }
+
+    let directory = &args[2];
+    let file_path = path.strip_prefix("/files/").unwrap_or("");
+    let full_path = format!("{}/{}", directory, file_path);
+
+    match fs::read(full_path) {
+        Ok(content) => {
+            format!(
+                "{}{}{}{}{}{}{}{}",
+                HTTP_OK,
+                CRLF,
+                CONTENT_TYPE_OCTET_STREAM,
+                CRLF,
+                format_args!("Content-Length: {}", content.len()),
+                CRLF,
+                CRLF,
+                &String::from_utf8_lossy(&content)
+            )
+        }
+        Err(_) => format!("{}{}{}", HTTP_NOT_FOUND, CRLF, CRLF),
     }
 }
